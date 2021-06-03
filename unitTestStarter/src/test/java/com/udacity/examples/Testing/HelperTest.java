8e66a90fd24f1f468995c81af1d8b147ce095cf9
annn <anna@nurzynska.com>
2021-06-03 13:42:55 +0200

    Added helper test methods.

--- unitTestStarter/src/test/java/com/udacity/examples/Testing/HelperTest.java
+++ unitTestStarter/src/test/java/com/udacity/examples/Testing/HelperTest.java
@@ -1,8 +1,62 @@
 package com.udacity.examples.Testing;
 
-import junit.framework.TestCase;
+import org.junit.*;
+
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+import java.util.List;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 public class HelperTest {
+    @Test
+    public void verify_getCount() {
+        List<String> empNames = Arrays.asList("sareeta", "", "john", "");
+        final long actual = Helper.getCount(empNames);
+        assertEquals(2, actual);
+    }
 
-	
+    @Test
+    public void verify_getStats() {
+        List<Integer> yrsOfExperience = Arrays.asList(13, 4, 15, 6, 17, 8, 19, 1, 2, 3);
+        List<Integer> actualList = Arrays.asList(13, 4, 15, 6, 17, 8, 19, 1, 2, 3);
+        IntSummaryStatistics stats = Helper.getStats(yrsOfExperience);
+        assertEquals(19, stats.getMax());
+        assertEquals(actualList, yrsOfExperience);
+    }
+
+    @Test
+    public void compare_arrays() {
+        int[] yrs = {10, 14, 2};
+        int[] expectedYrs = {10, 14, 2};
+        assertArrayEquals(expectedYrs, yrs);
+    }
+
+    @Test
+    public void verify_merged_list() {
+        List<String> empNames = Arrays.asList("sareeta", "", "john", "");
+        assertEquals("sareeta, john", Helper.getMergedList(empNames));
+    }
+
+    @Before
+    public void init() {
+        System.out.println("each method");
+    }
+
+    @BeforeClass
+    public static void setup() {
+        System.out.println("before class");
+    }
+
+    @After
+    public void initEnd() {
+        System.out.println("each method");
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        System.out.println("after class");
+    }
+
 }
